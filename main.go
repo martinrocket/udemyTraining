7779ef9ce9c4a3ef3c7bc2cffce00b242be60647
martinrocket <martin.ruble@gmx.com>
2017-02-12 21:59:32 -0700

    commit 5

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"time"
+)
 
 func main() {
 
 	fmt.Println("Program Start....")
+	makeCall()
+}
+
+func makeCall() {
+	for i := 1; i < 1000; i ++ {
+		fmt.Printf("%d \t %q \n", i, i)
+	}
 }
