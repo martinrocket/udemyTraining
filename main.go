0c1d5020a95045c3bdd546ece4397b1ec67e643d
martinrocket <martin.ruble@gmx.com>
2017-02-12 22:42:38 -0700

    sixth commmit

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 )
 
 func main() {
-
+	myInt := 10
 	fmt.Println("Program Start....")
 	makeCall()
+	fmt.Printf("%d \t %v \n", &myInt, &myInt)
+	fmt.Println(&myInt)
 }
 
 func makeCall() {
-	for i := 1; i < 1000; i ++ {
+	for i := 1; i < 10; i++ {
 		fmt.Printf("%d \t %q \n", i, i)
 	}
+
 }
